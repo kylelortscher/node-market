d9ca468ada546524996e758dd584b35e25879bfa
Kyle Lortscher <kylelortscher@gmail.com>
2016-04-18 11:40:57 -0700

    Removed modals for now, and setup login and signup as seprate pages, also started on setting up post routes for them

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 //Express Configuration
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs');
 //======================================
 //Profile Get
 //======================================
@@ -9,9 +10,58 @@ router.get('/profile', function(req,res){
 });
 
 //======================================
+//Login Get
+//======================================
+router.get('/login', function(req, res){
+   res.render('users/login');
+});
+
+//======================================
+//Signup Get
+//======================================
+router.get('/signup', function(req, res){
+    res.render('users/signup');
+});
+
+//======================================
 //Signup Post
 //======================================
+router.post('/signup', function(req, res) {
 
+    var password = req.body.password;
+    //TODO SETUP PASSWORD Check
+    if (password.length < 8) {
+        req.flash("error", "Password Must Be Longer Then 8 Characters");
+
+    }
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    var username = req.body.username;
+    var email = req.body.email;
+
+    //TODO Validate Email
+    //Checking For Proper Email
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    //TODO Validate Username
+    //Checking For Proper Username
+    function validateUsername(username) {
+        var re = /^[a-zA-Z0-9]+$/;
+        return re.test(username);
+    }
+
+    if(!validateEmail(email)) {
+        req.flash("error", "Not a valid Email Address!");
+        res.redirect("/")
+    } else if (!validateUsername(username)) {
+
+    }
+
+});
 
 //======================================
 //Login Post
