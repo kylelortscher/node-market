311f1d1a05c16fbe028b3851af76848b3d6bf781
Kyle Lortscher <kylelortscher@gmail.com>
2016-04-18 13:27:28 -0700

    Added middleware for login info

---
+++ middleware/index.js
@@ -0,0 +1,48 @@
+//Models
+var User = require("../models/user");
+
+//===============================
+//ALL MIDDLEWARE STORED HERE
+//===============================
+
+
+//EMPTY OBJECT TO PUT FUNCTIONS INSIDE
+var middlewareObj = {};
+
+//===============================
+//CHECK IF USER IS LOGGED IN
+//===============================
+middlewareObj.requireLogin = function (req, res, next){
+    if (!req.user) {
+        res.redirect('/login');
+    } else {
+        return next();
+    }
+};
+
+//===================================================
+//CHECK IF USER IS LOGGED IN DISPLAYING FLASH MESSAGE
+//===================================================
+middlewareObj.requireLoginFlash = function (req, res, next){
+    if (!req.user) {
+        req.flash("error", "You must be logged in!");
+        res.redirect('/login');
+    } else {
+        return next();
+    }
+};
+
+
+//===================================================
+//CHECK IF USER IS ALREADY LOGGED IN
+//===================================================
+middlewareObj.alreadyLoggedIn = function (req, res, next){
+    if(!req.user) {
+        next();
+    } else {
+        req.flash("error", "You are already logged in!");
+        res.redirect("/");
+    }
+};
+
+module.exports = middlewareObj;
