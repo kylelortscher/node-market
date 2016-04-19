402a747f086404a107cfcb6d3906db4c049142d1
Kyle Lortscher <kylelortscher@gmail.com>
2016-04-18 18:15:08 -0700

    Added a manage service page to manage the services made by the seller

--- routes/service.js
+++ routes/service.js
@@ -8,12 +8,26 @@ router.get('/show', function(req, res) {
     res.render('service/show');
 });
 
+
+//======================================
+//Show Certain User
+//======================================
 router.get('/user', function(req, res) {
    res.render('users/show');
 });
 
+//======================================
+//New Service
+//======================================
 router.get('/service/new', function(req, res){
    res.render('service/new');
+});
+
+//======================================
+//Manage Current Services
+//======================================
+router.get('/services/manage', function(req, res){
+    res.render('service/manage');
 });
 
 
