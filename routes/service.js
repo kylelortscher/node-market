48fdac1b2b178462204174464d0fe156cb30874f
Kyle Lortscher <kylelortscher@gmail.com>
2016-04-18 19:30:46 -0700

    Set up post to work for service

--- routes/service.js
+++ routes/service.js
@@ -1,6 +1,9 @@
 //Express Configuration
 var express = require('express');
 var router = express.Router();
+var Service = require("../models/service");
+
+
 //======================================
 //Show Service Get
 //======================================
@@ -36,13 +39,23 @@ router.get('/services/manage', function(req, res){
 router.post('/newservice', function(req, res){
     var title       = req.body.title;
     var price       = req.body.price;
-    var image       = req.file.image;
+    var image       = "https://static.pexels.com/photos/9416/light-car-display-shop-large.jpg";
     var youtubeUrl  = req.body.youtubeUrl;
     var category    = req.body.category;
     var description = req.body.description;
     var dueDate     = req.body.dueDate;
     var refund      = req.body.refund;
     var email       = req.user.email;
+
+    var newService = {title: title, price: price, image: image,youtubeUrl: youtubeUrl, category: category, description: description, dueDate: dueDate, refund: refund, email: email};
+    Service.create(newService, function(err, user){
+        if(err) {
+            console.log(err);
+        } else {
+            req.flash("info", "Your Service Has Been Posted!");
+            res.redirect('/');
+        }
+    });
 
 });
 
